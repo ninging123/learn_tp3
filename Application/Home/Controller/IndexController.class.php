e3c1bf4b047f1524213f15e43fb4073210285cfb
Ninging <972341480@qq.com>
2019-12-19 19:27:12 +0800

    添加仓库

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -6,18 +6,8 @@ class IndexController extends Controller {
         $this->show('<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} body{ background: #fff; font-family: "微软雅黑"; color: #333;font-size:24px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.8em; font-size: 36px } a,a:hover{color:blue;}</style><div style="padding: 24px 48px;"> <h1>:)</h1><p>欢迎使用 <b>ThinkPHP</b>！</p><br/>版本 V{$Think.version}</div><script type="text/javascript" src="http://ad.topthink.com/Public/static/client.js"></script><thinkad id="ad_55e75dfae343f5a1"></thinkad><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script>','utf-8');
     }
     public function pull(){
-//        $shell = "cd /www/wwwroot/learn_tp3/ && pwd && sudo git pull 2>&1";
-//        exec($shell,$out);
-        $out = array(
-            "/www/wwwroot/learn_tp3",
-            "From https://github.com/ninging123/learn_tp3",
-            "   f28fb48..56d6b2f  master     -> origin/master",
-            "error: Your local changes to the following files would be overwritten by merge:",
-            "	Application/Home/index.html",
-            "Please, commit your changes or stash them before you can merge.",
-            "Aborting",
-            "Updating f28fb48..56d6b2f"
-        );
+        $shell = "cd /www/wwwroot/learn_tp3/ && pwd && sudo git pull 2>&1";
+        exec($shell,$out);
         if ($this->rmm($out) == False){
             echo '<pre>';
             print_r($out);
